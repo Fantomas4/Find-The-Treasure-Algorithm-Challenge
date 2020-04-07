9c7e46bb886e0b8183f924e89b38f9db273bdb21
Fantomas4 <stefanokf@auth.gr>
2020-04-08 00:39:43 +0300

    Completed input file parser.

--- Mines.java
+++ Mines.java
@@ -1,6 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Mines {
 
+    List<int[]> minesPos = new ArrayList<>();
+    int[] startPos = new int[2];
+    int[] treasurePos = new int[2];
+
+    Mines(String inputFileDir) {
+        loadFile(inputFileDir);
+    }
+
+    private void loadFile(String dir) {
+        File file = new File(dir);
+        Scanner scanner;
+        String line;
+        String[] coordinates;
+
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+
+                if (!line.equals("")) {
+                    System.out.println(line);
+                    coordinates = line.split(" ");
+
+                    int[] point = new int[2];
+                    point[0] = Integer.parseInt(coordinates[0]);
+                    point[1] = Integer.parseInt(coordinates[1]);
+
+                    minesPos.add(point);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("*** Error: File not Found. ***");
+            return;
+        }
+
+        this.startPos = minesPos.remove(0);
+        this.treasurePos = minesPos.remove(minesPos.size() - 1);
+
+        System.out.println("================ DIAG ================");
+        System.out.printf("*** Starting position is: %d %d ***\n", this.startPos[0], this.startPos[1]);
+        System.out.printf("*** Treasure position is: %d %d ***\n", this.treasurePos[0], this.treasurePos[1]);
+        for (int[] point : this.minesPos) {
+            System.out.printf("%d %d\n", point[0], point[1]);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(args[0]);
+//        System.out.println(args[0]);
+        Mines mines = new Mines(args[0]);
+
+
     }
 }
