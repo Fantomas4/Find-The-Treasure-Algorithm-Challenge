9d11cdc364ee92b48cd12194d561301e2440e492
Fantomas4 <stefanokf@auth.gr>
2020-04-10 08:06:56 +0300

    Continued working on algorithm.

--- Mines.java
+++ Mines.java
@@ -10,6 +10,9 @@ public class Mines {
     int[] startPos = new int[2];
     int[] treasurePos = new int[2];
 
+    List<int[]> topSubHull = new ArrayList<>();
+    List<int[]> bottomSubHull = new ArrayList<>();
+
     Mines(String inputFileDir) {
         loadFile(inputFileDir);
     }
@@ -53,6 +56,32 @@ public class Mines {
         }
     }
 
+    private double pointsDistance(int[] point1, int[] point2) {
+        return Math.sqrt(Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point1[1], 2));
+    }
+
+    public int[] maxPointUsingAngle(int[] tp1, int[] tp2, int[] p1, int[] pn) {
+        double p1pnDistance = pointsDistance(p1, pn);
+
+        // Calculate the angle for test point 1 (tp1)
+        double tp1pnDistance = pointsDistance(tp1, pn);
+        double tp1p1Distance = pointsDistance(tp1, p1);
+        double angle1 = Math.acos((Math.pow(tp1pnDistance, 2) + Math.pow(tp1p1Distance, 2) - Math.pow(p1pnDistance, 2))
+                / (2 * tp1pnDistance * tp1p1Distance));
+
+        // Calculate the angle for test point 2 (tp2)
+        double tp2pnDistance = pointsDistance(tp2, pn);
+        double tp2p1Distance = pointsDistance(tp2, p1);
+        double angle2 = Math.acos((Math.pow(tp2pnDistance, 2) + Math.pow(tp2p1Distance, 2) - Math.pow(p1pnDistance, 2))
+                / (2 * tp2pnDistance * tp2p1Distance));
+
+        if (angle1 >= angle2) {
+            return tp1;
+        } else {
+            return tp2;
+        }
+    }
+
     //    ************ ATTENTION : slide 197, book pdf
     private int determinePointSide(int[] targetPoint, int[] linePoint1, int[] linePoint2) {
         int value = (targetPoint[1] - linePoint1[1]) * (linePoint2[0] - linePoint1[0]) -
@@ -75,11 +104,53 @@ public class Mines {
 
     private void quickhull(int[] p1, int[] pn, int side) {
 
+        int maxIndex = -1;
+        int maxDistance = 0;
+
+        //Find the point with the maximum distance from line p1pn
+        for (int i = 0; i < minesPos.size(); i++) {
+            int tempDistance = pointToLineDist(minesPos.get(i), p1, pn);
+            if (determinePointSide(minesPos.get(i), p1, pn) == side) {
+                if (tempDistance > maxDistance) {
+//                If the current mine point has a greater distance from p1pn than maxDistance
+                    maxIndex = i;
+                    maxDistance = tempDistance;
+                } else if (tempDistance == maxDistance) {
+                    // **** WIP: Calculate angle - slide 196
+                }
+            }
+        }
+
+        if (maxIndex == -1) {
+            //If no points are found, add p1 and pn to the
+            // appropriate Sub-Hull arraylist
+            if (side == 1) {
+                topSubHull.add(p1);
+                topSubHull.add(pn);
+            } else if (side == -1) {
+                bottomSubHull.add(p1);
+                bottomSubHull.add(pn);
+            }
+            return;
+        }
+
+        // Na afairw apo to minesPos osa stoixeia elegxw oste na meiwsw ton arithmo epanalipseon
+        // stiw epomenes anazitiseis quickhull?
+
+        quickhull(minesPos.get(maxIndex), p1, -determinePointSide(pn, minesPos.get(maxIndex), p1));
+        quickhull(minesPos.get(maxIndex), pn, -determinePointSide(p1, minesPos.get(maxIndex), pn));
     }
 
     public static void main(String[] args) {
 //        System.out.println(args[0]);
         Mines mines = new Mines(args[0]);
+
+//        int[] A = new int[]{0, 0};
+//        int[] B = new int[]{0, 1};
+//        int[] C = new int[]{1, 0};
+//
+//        mines.maxPointUsingAngle(A, new int[]{5, 5}, B, C);
+
 
 
     }
